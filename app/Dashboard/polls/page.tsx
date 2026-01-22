23a76d3bed1e0eeb632855545ba1ef053a0f1e07
Jhun0317 <act.carsido@gmail.com>
2026-01-22 09:29:59 +0800

    disable prerendering for dashboard polls page

---
+++ app/Dashboard/polls/page.tsx
@@ -0,0 +1,8 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+import PollsComponent from "@/components/dashboard/PollsComponent";
+
+export default function PollsPage() {
+  return <PollsComponent />;
+}
